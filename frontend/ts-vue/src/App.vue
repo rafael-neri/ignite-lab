6db960de8909e82682980d071e52c834dd72936b
Rafael Neri <rafepel@gmail.com>
2024-12-23 11:38:31 -0300

    Update Vue Version

---
+++ frontend/ts-vue/src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import HomePage from './components/HomePage.vue'
+</script>
+
+<template>
+  <HomePage />
+</template>
---
+++ frontend/ts-vue/src/components/HomePage.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1>Hello World!</h1>
+</template>
+
+<style scoped lang="scss">
+h1 {
+  color: #fff;
+}
+</style>
